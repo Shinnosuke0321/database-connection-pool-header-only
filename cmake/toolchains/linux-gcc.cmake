633c7f7d3aa4a7b85422515033ed66c3b5611ccf
Shinnosuke0321 <shinnosuke.kawai@shinnosukekawai.com>
2026-05-28 16:16:42 +0900

    added libstdc++ to gcc on linux

--- cmake/toolchains/linux-gcc.cmake
+++ cmake/toolchains/linux-gcc.cmake
@@ -1,2 +1,6 @@
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
+
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libstdc++")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-stdlib=libstdc++")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-stdlib=libstdc++")
